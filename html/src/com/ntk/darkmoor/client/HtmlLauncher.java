7bbfa63fb6a1c98b1eca5220b1d612154477da30
ntk <nikolaos.tsatsalmas@gmail.com>
2014-05-28 20:08:37 +0200

    Darkmoor empty project

---
+++ html/src/com/ntk/darkmoor/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.ntk.darkmoor.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.ntk.darkmoor.DarkmoorGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new DarkmoorGame();
+        }
+}
