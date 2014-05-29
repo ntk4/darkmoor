41d9ea5e2add9d98105e4d8d1ff15051bed51921
ntk <nikolaos.tsatsalmas@gmail.com>
2014-05-30 01:58:11 +0200

    Desktop mode displays the EOB2 game cover

--- core/src/com/ntk/darkmoor/DarkmoorGame.java
+++ core/src/com/ntk/darkmoor/DarkmoorGame.java
@@ -16,7 +16,7 @@ public class DarkmoorGame extends ApplicationAdapter {
 	public void create () {
 		resources = new Resources().loadResources("./data");
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("EOB2.jpg"); //size: 700x899
 	}
 
 	@Override
@@ -24,7 +24,7 @@ public class DarkmoorGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0, 700, 899);
 		batch.end();
 	}
 }
--- desktop/src/com/ntk/darkmoor/desktop/DesktopLauncher.java
+++ desktop/src/com/ntk/darkmoor/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.ntk.darkmoor.DarkmoorGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 899;
+		config.width = 700;
 		new LwjglApplication(new DarkmoorGame(), config);
 	}
 }
